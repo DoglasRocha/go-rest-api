cc6cbfffb453ec12b1c5aca76d6489387cf881bc
Doglas Rocha <79492860+DoglasRocha@users.noreply.github.com>
2022-06-30 22:14:27 -0300

    modularize the project using the MVC pattern and start the API

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"go-rest-api/models"
+	"go-rest-api/routes"
+)
+
+func main() {
+	models.Personalidades = []models.Personalidade{
+		{Nome: "Nome 1", Historia: "Historia 1"},
+		{Nome: "Nome 2", Historia: "Historia 2"},
+	}
+
+	routes.HandleRequest()
+}
