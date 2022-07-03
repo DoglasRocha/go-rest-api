575d70a853a1b590f1bc1eb2aa0c37e4a77bd175
Doglas Rocha <79492860+DoglasRocha@users.noreply.github.com>
2022-07-03 14:29:23 -0300

    connect to DB and use GORM

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"go-rest-api/database"
 	"go-rest-api/models"
 	"go-rest-api/routes"
 )
@@ -10,6 +11,7 @@ func main() {
 		{Id: 1, Nome: "Nome 1", Historia: "Historia 1"},
 		{Id: 2, Nome: "Nome 2", Historia: "Historia 2"},
 	}
+	database.ConectaComBancoDeDados()
 
 	routes.HandleRequest()
 }
